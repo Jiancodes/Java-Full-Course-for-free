64896eb50db8bf51c0fae2076cd050e05222ff8b
jian899 <mjian528@gmail.com>
2023-06-14 13:44:24 +0800

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/**创建你的第一个Java Application 程序HelloWorld.java*/
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		
+		System.out.println("Hello World!");
+	}
+	
+	
+	
+	
+	
+}
