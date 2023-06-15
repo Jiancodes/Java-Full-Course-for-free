e465c60c86077899d6728bac2df9730c565cba5a
jian899 <mjian528@gmail.com>
2023-06-15 11:09:27 +0800

    add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,22 @@
-/**鍒涘缓浣犵殑绗竴涓狫ava Application 绋嬪簭HelloWorld.java*/
+/*创建你的第一个Java Application 程序HelloWorld.java
+*   public: 公开的公共的，公开的的类名称必须和它所在的文件名保持一致
+*   class：类 
+*   {}：花括号就是类范围限定，括号以内的就是类的管辖范围
+*/
 public class HelloWorld
+/*  
+	static:静态的
+	void:没有返回值
+	main:程序入口函数的名称
+	String[]:字符串数组，形参类型
+	args:参数名称
+	{}：入口的范围
+	这是程序的入口
+*/
 {
 	public static void main(String[] args)
 	{
-		
+		// 这是输出语句，将括号里边引号中的内容直接打印在控制台上
 		System.out.println("Hello World!");
 	}
 	
